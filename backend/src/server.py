83a8a5afb9d95b3eda267f0af93d8f5392beeadf
Meshal Alamr <Meshal037@gmail.com>
2023-06-10 00:00:26 +0300

    Change backend to Flask instead of FastAPI, initial React form

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,14 +1,16 @@
-import io 
+import io
 import pytesseract
-import uvicorn
-
-from fastapi import FastAPI, Form, UploadFile, File
+from flask import Flask, request, jsonify
 from pdf2image import convert_from_bytes
-from typing import Dict, Optional, List, Annotated
+import base64
 
-app = FastAPI()
+app = Flask(__name__)
 
-def extract_text_from_pdf(pdf: bytes) -> str:
+pytesseract.pytesseract.tesseract_cmd = (
+    r"C:\Users\Meshal\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+)
+
+def extract_text_from_pdf(pdf):
     """
     A function to convert multi-page pdf to images
     @misc{PythonTe58:online,
@@ -20,34 +22,31 @@ def extract_text_from_pdf(pdf: bytes) -> str:
     note = {(Accessed on 06/01/2023)}
     }
     """
-    # Changed from path to bytes and pass pdf as byte data
     pages = convert_from_bytes(pdf)
-     
-    # Extract text from each page using Tesseract OCR
-    text_data = ''
+    text_data = ""
     for page in pages:
         text = pytesseract.image_to_string(page)
-        text_data += text + '\n'
-     
-    # Return the text data
+        text_data += text + "\n"
     return text_data
 
-@app.get('/status', status_code=200)
-async def status() -> Dict:
+@app.route('/status', methods=['GET'])
+def status():
     """
     A function to check the status of the server.
     """
-    return {'Status': 'OK!'}
+    return jsonify({'Status': 'OK!'})
 
-@app.post('/extract', status_code=200)
-async def extract(file: Annotated[bytes, File()]) -> Dict: 
+@app.route("/extract", methods=["POST"])
+def extract():
     """
     A function that extracts text from PDF documents.
     """
-    
-    pdf_text = extract_text_from_pdf(file)
-    return {'data': pdf_text}
+    data = request.get_json()
+    file_encoded = data["file"]
+    file_content = base64.b64decode(file_encoded)
+    print("extracting text from pdf...")
+    pdf_text = extract_text_from_pdf(file_content)
+    return jsonify({"data": pdf_text})
 
-    
-if __name__ == "__main__":
-    uvicorn.run('server:app', reload=True)
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
